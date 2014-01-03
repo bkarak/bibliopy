442b5e4a94d2cd7e18982554b5f6510056094775
Vassilios Karakoidas <vassilios.karakoidas@gmail.com>
2014-01-03 11:20:06 +0200

    added bibvalidator

--- bibtex/bibvalidator.py
+++ bibtex/bibvalidator.py
@@ -45,14 +45,36 @@ class BibFields(object):
     VALID_TYPES = ['article', 'book', 'booklet', 'inbook', 'incollection', 'inproceedings', 'manual', 'mastersthesis', 'misc', 'phdthesis', 'proceedings', 'techreport', 'unpublished']
     
     @staticmethod
+    def __get_required(btype):
+        __req = {'article': BibFields.ARTICLE_REQUIRED,
+                 'book': BibFields.BOOK_REQUIRED,
+                 'booklet': BibFields.BOOKLET_REQUIRED,
+                 'inbook': BibFields.INBOOK_REQUIRED,
+                 'incollection': BibFields.INCOLLECTION_REQUIRED,
+                 'inproceedings': BibFields.INPROCEEDINGS_REQUIRED,
+                 'manual': BibFields.MANUAL_REQUIRED,
+                 'mastersthesis': BibFields.MASTERTHESIS_REQUIRED,
+                 'misc': BibFields.MISC_REQUIRED,
+                 'phdthesis': BibFields.PHDTHESIS_REQUIRED,
+                 'proceedings': BibFields.PROCEEDINGS_REQUIRED,
+                 'techreport': BibFields.TECHREPORT_REQUIRED,
+                 'unpublished': BibFields.UNPUBLISHED_REQUIRED}
+                 
+        return __req.get(btype, None)
+    
+    @staticmethod
     def validate_entry(bibentry):
         if bibentry.btype not in VALID_TYPES:
             return False
         
-        if bibentry.btype == 'article':
-            for k in bibentry.keys():
-                pass
+        key_count = 0
+        required_list = BibFields.__get_required(bibentry.btype)
         
-        return False
-                
-    
+        if required_list is None:
+            return False
+        
+        for k in bibentry.keys():
+            if k in required_list:
+                key_count += 1
+        
+        return key_count == len(required_list)
