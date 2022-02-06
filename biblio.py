2b84dfe17f3823216e90f62bcd52cd74d9a4f779
Vassilios Karakoidas <bkarak@e-food.gr>
2022-02-06 14:17:21 +0200

    changed date to 2022

--- biblio.py
+++ biblio.py
@@ -2,7 +2,7 @@
 
 # biblio.py - A simple bibliography management tool
 
-# Copyright (c) 2012, Vassilios Karakoidas (vassilios.karakoidas@gmail.com)
+# Copyright (c) 2022, Vassilios Karakoidas (vassilios.karakoidas@gmail.com)
 # All rights reserved.
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
